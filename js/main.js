8a2e2b2ea990c836ef70e0e33e19106ed159da2f
Andi_Gl <69365869+Andy-usa@users.noreply.github.com>
2020-10-18 21:08:05 +0300

    andi

--- js/main.js
+++ js/main.js
@@ -5,4 +5,8 @@ $(function(){
         nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-right.svg" alt=""></button>',
         prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-left.svg" alt=""></button>'
    });
+
+   $('.menu__btn').on('click', function(){/* выводим кнопку через джаваскрипт */
+      $('.menu__list').slideToggle(); /* при клике выводить слайдер slideToggle -- функция джеквери */
+   }); 
 });
